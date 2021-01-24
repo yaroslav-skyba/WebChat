1c131517b571aaf9672ab8f433e1915c09d4ffb0
Yaroslav <yarunkan@gmail.com>
2021-01-24 19:24:06 +0200

    It works

--- src/main/java/io/github/yarunkan/webchat/server/Main.java
+++ src/main/java/io/github/yarunkan/webchat/server/Main.java
@@ -11,14 +11,15 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Set;
 
 public class Main {
-    private final List<String> messageList = new ArrayList<>();
-    private final List<String> clientsIdList = new ArrayList<>();
+    private final List<Map<String, String>> messageList = new ArrayList<>();
+    private final List<String> clientIdList = new ArrayList<>();
+    private Map<String, Boolean> isMessageSentMap = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         new Main().start();
@@ -28,29 +29,37 @@ public class Main {
         final int backlog = 0;
         final InetSocketAddress serverSocketAddress = new InetSocketAddress(80);
         final HttpServer server = HttpServer.create(serverSocketAddress, backlog);
+
         server.createContext("/", exchange -> {
             final String requestMethod = getRequestMethod(exchange);
-
             getRequestHeaders(exchange);
-
             String message = getRequestBody(exchange);
-
             getResponseHeaders(exchange);
 
-            if (requestMethod.equals("GET")) {
-                message = processGETRequest(exchange);
-            } else if (requestMethod.equals("POST")) {
-                processPOSTRequest(message);
-            }
+            message = processRequest(exchange, requestMethod, message);
 
             sendResponseHeaders(exchange, message);
-
             getResponseBody(exchange, message);
         });
+
         server.setExecutor(null);
         server.start();
 
         System.out.println("The server is started on port 80");
+    }
+
+    private String processRequest(HttpExchange exchange, String requestMethod, String message) {
+        final URI requestURI = exchange.getRequestURI();
+        final String query = requestURI.getQuery();
+        final Map<String, String> parsedQueryMap = parseQuery(query);
+
+        if (requestMethod.equals("GET")) {
+            message = processGETRequest(parsedQueryMap);
+        } else if (requestMethod.equals("POST")) {
+            processPOSTRequest(message, parsedQueryMap);
+        }
+
+        return message;
     }
 
     private String getRequestMethod(HttpExchange exchange) {
@@ -85,65 +94,74 @@ public class Main {
         }
     }
 
-    private void processPOSTRequest(String message) {
-        messageList.add(message);
+    private void processPOSTRequest(String message, Map<String, String> parsedQueryMap) {
+        final String id = parsedQueryMap.get("id");
+        final Map<String, String> messageMap = new HashMap<>();
+        messageMap.put(id, message);
+        messageList.add(messageMap);
+
         System.out.println(message);
+
+        isMessageSentMap = new HashMap<>();
+        for (String clientId : clientIdList) {
+            isMessageSentMap.put(clientId, false);
+        }
+        isMessageSentMap.put(id, true);
     }
 
-    private String processGETRequest(HttpExchange exchange) {
-        final URI requestURI = exchange.getRequestURI();
-        final String query = requestURI.getQuery();
-
+    private String processGETRequest(Map<String, String> parsedQueryMap) {
         StringBuilder response = new StringBuilder();
 
-        if (query != null) {
-            final Map<String, String> parsedQuery = parseQuery(query);
-            final String id = parsedQuery.get("id");
-            final int parsedId = parseId(id);
+        final String id = parsedQueryMap.get("id");
 
-            if (id.equals("unknown")) {
-                final String currentUserId = Integer.toString(clientsIdList.size());
-                clientsIdList.add(currentUserId);
+        if (id.equals("unknown")) {
+            final int clientIdListSize = clientIdList.size();
+            final String currentUserId = Integer.toString(clientIdListSize);
 
-                response = new StringBuilder(currentUserId + "\n");
+            clientIdList.add(currentUserId);
 
-                for (String message : messageList) {
-                    response.append(message).append("\n");
-                }
-            } else if (parsedId != -1) {
-                final String lastMessage;
+            response = new StringBuilder(currentUserId + "\n");
 
-                if (messageList.isEmpty()) {
-                    lastMessage = "";
-                } else {
-                    lastMessage = messageList.get(messageList.size() - 1);
-                }
+            for (Map<String, String> messageMap : messageList) {
+                final Map.Entry<String, String> messageEntry = getMessageEntry(messageMap);
+                final String message = messageEntry.getValue();
 
-                response = new StringBuilder(lastMessage);
+                response.append(message).append("\n");
             }
+        } else if (!id.equals("-1") && !messageList.isEmpty() && !isMessageSentMap.get(id)) {
+            isMessageSentMap.put(id, true);
+
+            final int messageListLastElementIndex = messageList.size() - 1;
+            final Map<String, String> messageMap = messageList.get(messageListLastElementIndex);
+            final Map.Entry<String, String> messageEntry = getMessageEntry(messageMap);
+            final String lastMessage = messageEntry.getValue();
+
+            response = new StringBuilder(lastMessage);
         }
 
         return response.toString();
     }
 
+    private Map.Entry<String, String> getMessageEntry(Map<String, String> stringStringMap) {
+        final Set<Map.Entry<String, String>> messageEntrySet = stringStringMap.entrySet();
+        final Iterator<Map.Entry<String, String>> messageEntryIterator = messageEntrySet.iterator();
+
+        return messageEntryIterator.next();
+    }
+
     @NotNull
     private Map<String, String> parseQuery(String query) {
         final Map<String, String> parsedQueryMap = new HashMap<>();
-        final String[] splitQueryStrings = query.split("=");
-        parsedQueryMap.put(splitQueryStrings[0], splitQueryStrings[1]);
 
-        return parsedQueryMap;
-    }
+        if (query != null) {
+            final String[] splitQueryTokens = query.split("&");
 
-    private int parseId(String id) {
-        final Pattern pattern = Pattern.compile("[0-9]");
-        final Matcher matcher = pattern.matcher(id);
-        final boolean isNumber = matcher.matches();
-
-        if (isNumber) {
-            return Integer.parseInt(id);
+            for (String splitQueryToken : splitQueryTokens) {
+                final String[] splitQuerySubTokens = splitQueryToken.split("=");
+                parsedQueryMap.put(splitQuerySubTokens[0], splitQuerySubTokens[1]);
+            }
         }
 
-        return -1;
+        return parsedQueryMap;
     }
 }
